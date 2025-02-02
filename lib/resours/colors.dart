76fc7b4c85064fc4059daa37569874673cf54d83
isaevabrorbek <87145089+abrorbek06@users.noreply.github.com>
2025-02-02 20:07:21 +0500

    .

---
+++ lib/resours/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const mainColor = Color(0xFF007A8C);
+  static const textColor = Color(0xFF336749);
+  static const secondryColor = Color(0xFF6C757D);
+  static const black = Color(0xFF000000);
+  static const bgColor = Color(0xFF001D3D);
+  static const white = Color(0xFFFFFFFF);
+  static const gray = Color(0xFF9F9B9B);
+  static const shadowColor = Color(0xFF787878);
+
+}
